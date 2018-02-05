224591e1f749c915d547f6ca5f78c3195efe54aa
Meilee <meilee@github.com>
2018-02-05 09:25:43 -0600

    Initial commit

---
+++ 1-types-and-variables.js
@@ -0,0 +1,6 @@
+let die1 = Math.floor(Math.random()*6+1);
+let diel2 = Math.floor(Math.random()*6+1);
+let total = diel1 +diel2;
+console.log(die1);
+console.log(die2);
+console.log("The total is:" + total);
