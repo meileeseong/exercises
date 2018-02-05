76e2647c65efc03909e9d8a4ad00dde470e78bc4
Meilee <meilee@github.com>
2018-02-05 09:28:58 -0600

    corrections

--- 1-types-and-variables.js
+++ 1-types-and-variables.js
@@ -1,6 +1,6 @@
 let die1 = Math.floor(Math.random()*6+1);
-let diel2 = Math.floor(Math.random()*6+1);
-let total = diel1 +diel2;
+let die2 = Math.floor(Math.random()*6+1);
+let total = die1 +die2;
 console.log(die1);
 console.log(die2);
 console.log("The total is:" + total);
